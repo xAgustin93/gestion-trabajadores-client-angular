ef39c04a1a73da54bdad9cca29fe1e00896f5d40
Agustin Navarro <xAgustin93@gmail.com>
2019-04-07 20:58:30 +0200

    Seccion Home

--- src/app/components/dashboard/home/home.component.ts
+++ src/app/components/dashboard/home/home.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public title: String;
+
   constructor() { }
 
   ngOnInit() {
+
+    this.title = 'Home';
   }
 
 }
--- src/app/components/menu-left/menu-left.component.ts
+++ src/app/components/menu-left/menu-left.component.ts
@@ -6,6 +6,7 @@ import { faLightbulb } from '@fortawesome/free-solid-svg-icons';
 import { faHourglass } from '@fortawesome/free-solid-svg-icons';
 import { faUsers } from '@fortawesome/free-solid-svg-icons';
 import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faFileAlt } from '@fortawesome/free-solid-svg-icons';
 
 // Services
 import { FunctionsServices } from '../../services/functions.service';
@@ -30,6 +31,7 @@ export class MenuLeftComponent implements OnInit {
   public faHourglass = faHourglass;
   public faUsers = faUsers;
   public faHome = faHome;
+  public faFileAlt = faFileAlt;
 
   constructor(
     private _functionsServices: FunctionsServices
